8610cb4d0eacb97e5b3aeae09e73224756836250
Cillian Cooke <cilliancooke876@gmail.com>
2026-02-16 13:49:33 +0000

    book design change

--- lib/book_widget.dart
+++ lib/book_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class BookWidget extends StatelessWidget {
+class BookWidget extends StatefulWidget {
   final String title;
   final double width;
   final double height;
   final VoidCallback? onTap;
-  
+
   const BookWidget({
-    super.key, 
+    super.key,
     required this.title,
     required this.width,
     required this.height,
@@ -15,22 +15,122 @@ class BookWidget extends StatelessWidget {
   });
 
   @override
+  State<BookWidget> createState() => _BookWidgetState();
+}
+
+class _BookWidgetState extends State<BookWidget> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap ?? () {
-        print('Tapped on $title');
-      },
-      child: SizedBox(
-        width: width,
-        height: height,
-        child: Image(
-          image: ResizeImage(
-            AssetImage('assets/book.png'),
-            width: 40,
-            height: 60,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: widget.width,
+          height: widget.height,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001)
+            ..rotateY(_isHovered ? -0.1 : 0),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                top: 0,
+                bottom: 0,
+                child: Container(
+                  width: widget.width * 0.1,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Colors.brown.shade800,
+                        Colors.brown.shade600,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: widget.width * 0.08,
+                top: 0,
+                bottom: 0,
+                right: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.amber.shade700,
+                        Colors.amber.shade900,
+                      ],
+                    ),
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(4),
+                      bottomRight: Radius.circular(4),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 8,
+                        offset: const Offset(4, 4),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.title,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: widget.width * 0.12,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: const Offset(1, 1),
+                                blurRadius: 2,
+                              ),
+                            ],
+                          ),
+                          textAlign: TextAlign.center,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 0,
+                top: 4,
+                bottom: 4,
+                child: Container(
+                  width: 3,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.grey.shade300,
+                        Colors.grey.shade100,
+                        Colors.grey.shade300,
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          filterQuality: FilterQuality.none,
-          fit: BoxFit.fill,
         ),
       ),
     );
