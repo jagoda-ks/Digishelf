fb80eb9469fd1ffe9d94e272164f13fb72722b71
IceryDev <71143180+IceryDev@users.noreply.github.com>
2026-02-27 22:41:26 +0000

    Fetch Cover, Publication Date, etc.

--- lib/book_utils.dart
+++ lib/book_utils.dart
@@ -1,10 +1,11 @@
+import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() async{
   
   BookInfo a = await Utils.fetchBook("9786055794804");
-  print(a.title);
+  print(a.pubDate);
 
 }
 
@@ -14,10 +15,10 @@ class BookInfo {
   String title;
   String author;
   String pubDate;
-  //Uint8List? cover;
+  Uint8List? cover;
   
 
-  BookInfo(this.title, this.author, this.pageCount, this.pubDate, this.isbn);
+  BookInfo(this.title, this.author, this.pageCount, this.pubDate, this.isbn, this.cover);
 }
 
 class Utils{
@@ -25,15 +26,23 @@ class Utils{
   Utils._();
 
   static const double widthPerPage = 2; //Temp value
+  static const int defaultPageCount = 50; //If returns null
 
   static double getWidth(double pages) => widthPerPage * pages;
 
   static Future<BookInfo> fetchBook(String isbn) async{
-    final results = await http.get(Uri.parse('https://openlibrary.org/isbn/$isbn.json'));
+    final results = await http.get(Uri.parse('https://openlibrary.org/api/books?bibkeys=ISBN:$isbn&jscmd=data&format=json'));
     final Map<String, dynamic> info = jsonDecode(results.body);
-    //final cover = results.bodyBytes;
-    BookInfo temp = BookInfo(info["title"], 
-                             'temp', info["number_of_pages"]??0, info["publish_date"]??0, isbn);
+    final Map<String, dynamic> infoUnwrapped = info["ISBN:$isbn"];
+
+
+    final coverResponse = await http.get(Uri.parse(infoUnwrapped["cover"]["medium"]));
+    final Uint8List coverBytes = coverResponse.bodyBytes;
+    BookInfo temp = BookInfo(infoUnwrapped["title"], 
+                             infoUnwrapped["authors"][0]["name"], 
+                             infoUnwrapped["number_of_pages"]??defaultPageCount, 
+                             infoUnwrapped["publish_date"]??"Unknown", 
+                             isbn, coverBytes);
     return temp;
   }
 
